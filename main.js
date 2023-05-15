741abbf1fc1aaf12466103a34039c4f4e62052eb
Hayoon Cho <117485593+laurenCho9@users.noreply.github.com>
2023-05-15 23:24:00 +0900

    Add: Task button 기능 추가 완료

--- main.js
+++ main.js
@@ -3,4 +3,23 @@ let addButton = document.getElementById("add-button");
 let taskList = [];
 addButton.addEventListener("click", addTask);
 
-function addTask() {}
+function addTask() {
+  let taskContent = taskInput.value;
+  taskList.push(taskContent);
+  console.log(taskList);
+  render();
+}
+
+function render() {
+  let resultHTML = "";
+  for (let i = 0; i < taskList.length; i++) {
+    resultHTML += `<div class="task">
+    <div>${taskList[i]}</div>
+      <div>
+        <button>Check</button>
+        <button>Delete</button>
+      </div>
+    </div>`;
+  }
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
