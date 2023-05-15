4228b68d2501cec2ba910323b7522a26010bfb1c
laurenCho9 <lauren.choux@gmail.com>
2023-05-15 22:08:35 +0900

    Add: Task button 중간푸쉬

---
+++ main.js
@@ -0,0 +1,6 @@
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let taskList = [];
+addButton.addEventListener("click", addTask);
+
+function addTask() {}
