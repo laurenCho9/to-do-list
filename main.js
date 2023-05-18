357cffabf47abea8d2112ffd1545c8e6ab5cecf1
Hayoon Cho <117485593+laurenCho9@users.noreply.github.com>
2023-05-18 23:45:16 +0900

    Add: 아이템 삭제하기, 아이템 필터링하기

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
 let taskList = [];
+let mode = "all";
+let filterList = [];
 addButton.addEventListener("click", addTask);
+
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
+console.log(tabs);
 
 function addTask() {
   let task = {
@@ -15,22 +25,29 @@ function addTask() {
 }
 
 function render() {
+  let list = [];
+  if (mode == "all") {
+    list = taskList;
+  } else if (mode == "ongoing" || mode == "done") {
+    list = filterList;
+  }
+
   let resultHTML = "";
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete == true) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
       resultHTML += `<div class="task">
-      <div class="task-done">${taskList[i].taskContent}</div>
+      <div class="task-done">${list[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-          <button onclick="deleteTask()">Delete</button>
+          <button onclick="toggleComplete('${list[i].id}')">Check</button>
+          <button onclick="deleteTask('${list[i].id}')">Delete</button>
         </div>
       </div>`;
     } else {
       resultHTML += `<div class="task">
-      <div>${taskList[i].taskContent}</div>
+      <div>${list[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-          <button onclick="deleteTask()">Delete</button>
+          <button onclick="toggleComplete('${list[i].id}')">Check</button>
+          <button onclick="deleteTask('${list[i].id}')">Delete</button>
         </div>
       </div>`;
     }
@@ -49,8 +66,46 @@ function toggleComplete(id) {
   console.log(taskList);
 }
 
-function deleteTask() {
-  console.log("삭제하자.");
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
+      break;
+    }
+  }
+  render();
+}
+
+function filter(event) {
+  mode = event.target.id;
+  filterList = [];
+
+  document.getElementById("under-line").style.width =
+    event.target.offsetWidth + "px";
+  document.getElementById("under-line").style.top =
+    event.target.offsetTop + event.target.offsetHeight + "px";
+  document.getElementById("under-line").style.left =
+    event.target.offsetLeft + "px";
+
+  if (mode == "all") {
+    render();
+  } else if (mode == "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filterList.push(taskList[i]);
+      }
+    }
+
+    render();
+  } else if (mode == "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
+  console.log(filterList);
 }
 
 function randomIDGenerate() {
